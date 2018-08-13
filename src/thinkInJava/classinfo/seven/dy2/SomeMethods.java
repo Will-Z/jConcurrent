003d0dda259c3bcb57215fc9a26604c05f41916b
Will-Z <evilliril@gmail.com>
2018-08-13 20:17:50 +0800

    dy2

---
+++ src/thinkInJava/classinfo/seven/dy2/SomeMethods.java
@@ -0,0 +1,15 @@
+package thinkInJava.classinfo.seven.dy2;
+
+/**
+ * @author Will
+ * @date 2018/8/13.
+ */
+public interface SomeMethods {
+    void boring1();
+
+    void boring2();
+
+    void interesting(String arg);
+
+    void boring3();
+}
